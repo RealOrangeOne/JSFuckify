c6c99a0ca1c6f5eb1ccbcd17fa9303214f08b629
Jake Howard <git@theorangeone.net>
2016-03-25 12:28:28 +0000

    Add tests

---
+++ scripts/test-helper.js
@@ -0,0 +1,5 @@
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+
+chai.expect();
+chai.use(sinonChai);
---
+++ tests/basic.test.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+
+describe('Basic Mocha Setup', function () {
+  it('adds two and two and gets four', function () {
+    expect(2 + 2).to.equal(4);
+  });
+});
